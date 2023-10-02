5a5c3e16c373a58f2a72c39c86ea853259b3d051
Mazen Salah <masalah.tech@gmail.com>
2023-10-02 07:32:04 +0300

    #

--- FootballFanPlatform/Program.cs
+++ FootballFanPlatform/Program.cs
@@ -1,7 +1,12 @@
+using FootballFanPlatform.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<AppDbContext>(
+    o => o.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
