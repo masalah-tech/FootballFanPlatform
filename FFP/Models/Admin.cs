dc095c66250783774bd76201232aa36c00677652
Mazen Salah <masalah.tech@gmail.com>
2024-08-31 16:59:13 +0300

    Remove labels and some changes

--- FFP/Models/Admin.cs
+++ FFP/Models/Admin.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace FFP.Models
@@ -11,28 +12,35 @@ namespace FFP.Models
         public string Username { get; set; }
         [Required(ErrorMessage = "Password is required.")]
         [RegularExpression(@"^(?=.*[A-Za-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,20}$", ErrorMessage = "Password must be 8-20 characters long, contain at least one uppercase letter, one lowercase letter, one number, and one special character.")]
+        [DisplayName("Password")]
         public string EncPassword { get; set; }
         [Required]
         [ForeignKey("AdminRole")]
+        [DisplayName("Role")]
         public int AdminRoleId { get; set; }
         public AdminRole AdminRole { get; set; }
         [Required]
+        [DisplayName("First Name")]
         public string FirstName { get; set; }
         [Required]
+        [DisplayName("Second Name")]
         public string SecondName { get; set; }
         [Required]
+        [DisplayName("Third Name")]
         public string ThirdName { get; set; }
         [Required]
+        [DisplayName("Last Name")]
         public string LastName { get; set; }
         [Required]
         [Column(TypeName = "Date")]
-        [Range(typeof(DateTime), "1900-01-01", "2024-12-31", ErrorMessage = "Birth Date must be between January 1, 1900, and December 31, 2024.")]
+        [DisplayName("Birth Date")]
         public DateTime BirthDate { get; set; } = new DateTime(2000, 1, 1);
         [Required]
         public string PersonalPhotoPath { get; set; }
         [Required]
         public string Email { get; set; }
         [Required]
+        [DisplayName("Phone Number")]
         public string PhoneNumber { get; set; }
         [Required]
         public string Nationality { get; set; }
